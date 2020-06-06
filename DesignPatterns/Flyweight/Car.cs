fc1ec425f117791e3f18e11a588ccba9e0e134ea
Varun Verma <vermavarun@outlook.com>
2020-06-06 17:14:35 +0530

    Flyweight pattern added.

---
+++ DesignPatterns/Flyweight/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Flyweight
+{
+    public class Car
+    {
+        public string Owner { get; set; }
+
+        public string Number { get; set; }
+
+        public string Company { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+    }
+}
