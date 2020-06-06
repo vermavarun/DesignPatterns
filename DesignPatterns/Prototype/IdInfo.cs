dd4e8b111aca87f331e8b22ed82a8ee79f3976f8
Varun Verma <vermavarun@outlook.com>
2020-06-06 17:04:37 +0530

    prototype pattern added

---
+++ DesignPatterns/Prototype/IdInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
